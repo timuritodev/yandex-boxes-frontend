f80cb9b44e97ad1245bf7d5ce843b8fd7a3f8235
luciD <sosalnet@yandex.ru>
2023-06-06 18:28:05 +0300

    renamed scans

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import Header from "../Header/Header";
+import OrderInformation from "../OrderInformation/OrderInformation";
+import ProblemButton from "../ProblemButton/ProblemButton";
+import CloseButton from "../CloseButton/CloseButton";
 
 function App() {
   return (
-    <Header />
-
+    <>
+      <Header />
+      <OrderInformation />
+      <ProblemButton />
+      <CloseButton />
+    </>
   );
 }
 
