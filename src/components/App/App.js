f4fbbebd10152d5ab5d9c2f25f121ecb557cb0d8
luciD <sosalnet@yandex.ru>
2023-06-06 17:55:35 +0300

    problem and close buttons

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,10 @@
 import "./App.css";
 import Header from "../Header/Header";
-import Scans from "../Scans/Scans";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Scans />
-    </>
+    <Header />
+
   );
 }
 
