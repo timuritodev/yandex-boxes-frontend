b68baa75939befc4f7c8483de9622e61115a3580
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2023-06-06 21:07:31 +0400

    position header in App component

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,14 +1,15 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Header from "../Header/Header";
+import Main from "../Main/Main";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
-        <Header />
         <Route path="/" element={<h2>стартовый компонент</h2>} />
-        <Route path="main" element={<h2>компонент основной</h2>} />
+        <Route path="main" element={<Main />} />
         <Route path="problem" element={<h2>компонент есть проблема</h2>} />
         <Route path="*" element={<h2>Страницы не существует</h2>} />
       </Routes>
