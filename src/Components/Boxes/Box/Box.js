4e92c38485fcc762be3f4ae8afd7400ce31bf6cd
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2023-06-09 16:13:49 +0400

    added styles

--- src/Components/Boxes/Box/Box.js
+++ src/Components/Boxes/Box/Box.js
@@ -1,43 +1,10 @@
 import "./Box.css";
+import { styleBoxesMap } from "../../../utils/constants";
 
 function Box({ name }) {
-  const styleMap = {
-    "Коробка YMA": {
-      boxColor: "carton_blue",
-      textColor: "carton__name_blue",
-    },
-    "Коробка YMC": {
-      boxColor: "carton_green",
-      textColor: "carton__name_green",
-    },
-    "Пакет MYC": {
-      boxColor: "carton_lightblue",
-      textColor: "carton__name_lightblue",
-    },
-    "Коробка YMF": {
-      boxColor: "carton_orange",
-      textColor: "carton__name_orange",
-    },
-    "Коробка MYF": {
-      boxColor: "carton_purple",
-      textColor: "carton__name_purple",
-    },
-    "Пакет MYA": { boxColor: "carton_red", textColor: "carton__name_red" },
-    "Пакет MYD": { boxColor: "carton_gray", textColor: "carton__name_gray" },
-    "Пакет MYB": {
-      boxColor: "carton_darkred",
-      textColor: "carton__name_darkred",
-    },
-    "Пакет MYE": {
-      boxColor: "carton_darkyellow",
-      textColor: "carton__name_darkyellow",
-    },
-  };
-
-  const { boxColor, textColor } = styleMap[name] || {};
+  const { boxColor, textColor } = styleBoxesMap[name] || {};
 
   const boxClasses = `carton ${boxColor}`;
-
   const textClasses = `carton__name ${textColor}`;
 
   return (
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -46,5 +46,32 @@ const boxData = [
   },
 ];
 
+const styleBoxesMap = {
+  "Коробка YMA": { boxColor: "carton_blue", textColor: "carton__name_blue" },
+  "Коробка YMC": { boxColor: "carton_green", textColor: "carton__name_green" },
+  "Пакет MYC": {
+    boxColor: "carton_lightblue",
+    textColor: "carton__name_lightblue",
+  },
+  "Коробка YMF": {
+    boxColor: "carton_orange",
+    textColor: "carton__name_orange",
+  },
+  "Коробка MYF": {
+    boxColor: "carton_purple",
+    textColor: "carton__name_purple",
+  },
+  "Пакет MYA": { boxColor: "carton_red", textColor: "carton__name_red" },
+  "Пакет MYD": { boxColor: "carton_gray", textColor: "carton__name_gray" },
+  "Пакет MYB": {
+    boxColor: "carton_darkred",
+    textColor: "carton__name_darkred",
+  },
+  "Пакет MYE": {
+    boxColor: "carton_darkyellow",
+    textColor: "carton__name_darkyellow",
+  },
+};
+
 // eslint-disable-next-line import/prefer-default-export
-export { boxData };
+export { boxData, styleBoxesMap };
